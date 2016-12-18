0ca2819356d3d8d221332158b68237c40a7e5d94
Daniel Porteous <danielporteous1@gmail.com>
2016-12-18 22:13:04 +1100

    Run "where_should_i_be_up_to.py rev" to see a revised plan.
    Since I fell behind, this new plan omits the last two chapters, which are
    about applying the learned topics, whereas up to this point it was all
    still learning.

--- where_should_i_be_up_to.py
+++ where_should_i_be_up_to.py
@@ -3,13 +3,25 @@
 # Tells me what page I should be up to by the end of the day.
 
 from datetime import date
+from sys import argv
+from math import ceil
 
 startDate = date(2016, 11, 27)
 startPage = 90
-pageChunk = 20 
-# Down from 25. This is not conservative, you *must* do the 20 pages to keep up.
-# This is page 844 - page 90, divided by 39 days (27th of Nov to 5th of Jan).
+endDate = date(2017, 1, 5)
+endPage = 844
 
+# I stopped for a few days and fell too far behind, so this
+# is a revised schedule which omits the last 2 chapters.
+if len(argv) > 1:
+    if argv[1] == "revised" or argv[1] == "rev":
+        startDate = date(2016, 12, 18)
+        startPage = 405
+        endPage = 742
+
+# We calculate the pageChunk now, instead of specifying it in advance.
+# It should still be 20 regardless of which of the dates/pages you choose.
+pageChunk = ceil((endPage - startPage) / (endDate - startDate).days)
 daysSinceStart = (date.today() - startDate).days
 target = startPage + daysSinceStart * pageChunk
 print(target)
