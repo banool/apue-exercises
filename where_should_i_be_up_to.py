b7a19f92fa9521bd75cb39f0223f4655c7caa532
Daniel Porteous <danielporteous1@gmail.com>
2016-12-24 23:29:18 +1100

    Can now take an offset (in days).

--- where_should_i_be_up_to.py
+++ where_should_i_be_up_to.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
 
 # Tells me what page I should be up to by the end of the day.
+# Can take an offset. Will not check for silly offsets like -1000.
 
 from datetime import date
 from sys import argv, exit
 from math import ceil
 
 def printUsage():
-	print("USAGE: {} <schedule>".format(argv[0]))
+	print("USAGE: {} <schedule or offset>".format(argv[0]))
 	print("  <schedule>: \"rev\" or \"orig\". \"rev\" is default.")
+	print("  <offset>:   +n or -n. n must be an integer")
 	exit(1)
 
+def getOffset(arg1):
+	try:
+		offset = int(arg1[1:])
+		if arg1[0] == "-":
+			offset = -offset
+	except:
+		printUsage()
+	return offset
+
 endDate = date(2017, 1, 5)
+offset = 0
 
 # I stopped for a few days and fell too far behind, so this
 # is a revised schedule which omits the last 2 chapters.
@@ -19,6 +31,10 @@ if len(argv) == 1:
     argv.append("rev")
 if len(argv) not in (1,2):
 	printUsage()
+
+if "+" in argv[1] or "-" in argv[1]:
+	offset = getOffset(argv[1])
+	argv[1] = "rev"
 
 if argv[1] == "revised" or argv[1] == "rev":
     startDate = date(2016, 12, 18)
@@ -34,6 +50,6 @@ else:
 # We calculate the pageChunk now, instead of specifying it in advance.
 # It should still be 20 regardless of which of the dates/pages you choose.
 pageChunk = ceil((endPage - startPage) / (endDate - startDate).days)
-daysSinceStart = (date.today() - startDate).days
+daysSinceStart = (date.today() - startDate).days + offset
 target = startPage + daysSinceStart * pageChunk
 print(target)
